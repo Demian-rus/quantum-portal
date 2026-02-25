8f3ede3ec1b122f85097e7ea594c22b347166e22
Demian-rus <dima.usanov45@gmail.com>
2026-02-25 20:08:25 +0300

    Add files via upload

--- core.js
+++ core.js
@@ -583,7 +583,7 @@ setTimeout(() => {
 // ==========================================
 document.addEventListener('contextmenu', event => event.preventDefault());
 document.onkeydown = function(e) {
-  if(e.keyCode == 123) return false;
+  if(e.keyCode == 123) return false; // F12
   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
   if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) return false;
   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) return false;
